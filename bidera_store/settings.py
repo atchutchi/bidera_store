d18fbb80cacdaaca27463b1f90bb398e3873c536
Atchutchi Ferreira <ferreira.atchutchi@gmail.com>
2024-03-15 10:58:55 +0000

    Added contact form, views, template and style to the contact app

--- bidera_store/settings.py
+++ bidera_store/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'checkout',
     'profiles',
     'wishlist',
+    'contact',
 
     # Other
     'crispy_forms',
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib import messages
 
-# Create your views here.
+# The contact view for handling the contact form submission
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # Optionally save form to the database
+            form.save()
+            
+            # Send an email
+            send_mail(
+                form.cleaned_data['subject'],
+                form.cleaned_data['message'],
+                settings.DEFAULT_FROM_EMAIL,
+                ['biderastore@gmail.com'],
+            )
+            
+            # Add a success message for on-page confirmation
+            messages.success(request, 'Your message has been sent successfully. We will get back to you shortly.')
+            
+            # Redirect back to the contact page, which will show the success message
+            return redirect('contact')
+    else:
+        form = ContactForm()
+
+    # Render the contact page with the ContactForm instance
+    return render(request, 'contact/contact.html', {'form': form})
