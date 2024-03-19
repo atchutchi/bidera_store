7ab92050d6f9302fc9559551b3063bf1805c25bb
Atchutchi Ferreira <ferreira.atchutchi@gmail.com>
2024-03-19 16:53:15 +0000

    Refactoring with flake8 and added 404page

--- bidera_store/urls.py
+++ bidera_store/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +32,5 @@ urlpatterns = [
     path('contact/', include('contact.urls')),
     path('newsletter/', include('newsletter.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+handler404 = 'bidera_store.views.handler404'
+
