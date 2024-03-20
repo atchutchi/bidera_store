dac25fe2c87fe086189d7482ae1d1025867f5bd0
atchutchi <ferreira.atchutchi@gmail.com>
2024-03-20 21:14:40 +0000

    Removed app newsletter

--- newsletter/context_processors.py
+++
@@ -1,7 +0,0 @@
-from .forms import NewsletterSignupForm
-
-
-def newsletter_signup(request):
-    form = NewsletterSignupForm()
-    return {'newsletter_signup_form': form}
-
--- newsletter/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import NewsletterSignup
-
-
-class NewsletterSignupForm(forms.ModelForm):
-    class Meta:
-        model = NewsletterSignup
-        fields = ['email']
-
