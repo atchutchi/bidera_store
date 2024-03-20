613cc4195be24792c58f17561f17222a6b746a55
atchutchi <ferreira.atchutchi@gmail.com>
2024-03-20 21:16:23 +0000

    Uncommented secretkey

--- bidera_store/settings.py
+++ bidera_store/settings.py
@@ -23,8 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY', '')
-SECRET_KEY = '2nvmv(++5&imqfy&s=68#9%(7y2l+z!%s75i=rk08+-==we&)6'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+# SECRET_KEY = '2nvmv(++5&imqfy&s=68#9%(7y2l+z!%s75i=rk08+-==we&)6'
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
