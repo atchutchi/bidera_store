04bc5bb136082ba442178615ba3003434503ebb4
atchutchi <ferreira.atchutchi@gmail.com>
2024-03-22 06:28:00 +0000

    debug change

--- bidera_store/settings.py
+++ bidera_store/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 
 # DEBUG = 'DEVELOPMENT' in os.environ
 
