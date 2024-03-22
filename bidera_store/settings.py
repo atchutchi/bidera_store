aae7007f53bb2084656e3f222a792478c0749613
atchutchi <ferreira.atchutchi@gmail.com>
2024-03-22 07:02:34 +0000

    Changed debug

--- bidera_store/settings.py
+++ bidera_store/settings.py
@@ -24,11 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+# SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
+SECRET_KEY = '2nvmv(++5&imqfy&s=68#9%(7y2l+z!%s75i=rk08+-==we&)6'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 # DEBUG = 'DEVELOPMENT' in os.environ
 
