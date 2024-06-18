c7adf4bc7c1cd624854f86cfbafb8eba32447a99
Atchutchi Ferreira <ferreira.atchutchi@gmail.com>
2024-06-18 09:37:11 +0000

    Changed the debug to false

--- bidera_store/settings.py
+++ bidera_store/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'default_development_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 # DEBUG = 'DEVELOPMENT' in os.environ
 
